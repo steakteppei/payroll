5c9042beed1483a0aef439cf0d329e86de849d82
steakteppei <aki@teppei-usa.com>
2026-06-19 02:11:20 -1000

    Add files via upload

--- sw-home.js
+++ sw-home.js
@@ -1,5 +1,5 @@
 // ★ バージョンをhome.htmlのAPP_VERSIONと合わせて更新する
-var CACHE = 'st-home-v3.2';
+var CACHE = 'st-home-v3.3';
 var FILES = ['./home.html', './payroll.html', './version.html', './payroll-summary.html', './config.js'];
 
 self.addEventListener('install', function(e) {
